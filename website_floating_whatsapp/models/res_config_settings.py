40b0b943c377407f72c2b5087b910c51259715e8
antoniofregoso <antoniofregoso@ai>
2019-03-15 22:59:26 -0600

    [FIX] Configuration form

--- website_floating_whatsapp/models/res_config_settings.py
+++ website_floating_whatsapp/models/res_config_settings.py
@@ -6,4 +6,4 @@ from odoo import api, fields, models, tools
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     
-    floating_whatsapp_id = fields.Many2one('website.floating_whatsapp', related="website_id.floating_whatsapp_id")
+    floating_whatsapp_id = fields.Many2one('website.floating_whatsapp', related="website_id.floating_whatsapp_id", string='WhaysApp Template', readonly=False)
